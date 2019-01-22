ca64d55352a458b7406ce219f510d9b3337498cf
abbiyanaila <abbiyanaila@gmail.com>
2019-01-22 18:42:24 +0800

    git all library.python

--- dirfile/os_opration.py
+++ dirfile/os_opration.py
@@ -1,4 +1,24 @@
 import os
 
-ld = os.listdir('/home')
-print(ld)
+print(f"Listing Directory : {os.listdir('/home')}")
+print(f'Counting CPU : {os.cpu_count()}')
+print(f'Where is My current working directory? : {os.getcwd()}')
+print(f"Get my Directory name from /home/abbiyanaila {os.path.dirname('/home/abbiyanaila')}")
+print(f"Check The existence of directory /home/abbiyanaila: {os.path.exists('/home/abbiyanaila')}")
+print(f"Join path directory : {os.path.join('/home','desi','test')}")
+
+#detele operation with os
+mypath = os.getcwd()
+myfile = os.path.join(mypath, 'test.txt')
+if os.path.exists(myfile):
+     os.unlink(myfile)
+
+
+# create directory recursively
+mypath = os.getcwd()
+mydir = os.path.join(mypath, 'testdir','test_again','again')
+os.makedirs(mydir, exist_ok=True)
+
+#755
+import stat
+os.chmod(mydir, stat.S_IRWXG)
